ac69711d2c376abd59872f9f02d8330c9b69a1eb
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2024-09-18 15:14:22 +0200

    while + random

--- Fighting/Program.cs
+++ Fighting/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+while (true)
+{
+  int hp = Random.Shared.Next(50, 100);
+  Console.WriteLine(hp);
+  Console.ReadLine();
+}
+
+Console.WriteLine("Skriv ditt namn:");
+string name = Console.ReadLine();
+
+while (name.Length == 1)
+{
+  Console.WriteLine("Du kan ju inte bara trycka enter, dumhuvve");
+  Console.WriteLine("Försök igen:");
+  name = Console.ReadLine();
+}
+
+
+
+Console.ReadLine();
